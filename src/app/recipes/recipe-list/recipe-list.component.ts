8ae53c60faf668ff08781a63cd8841fa9bd99759
Neel1204 <neel9770635705@gmail.com>
2021-04-17 17:22:03 -0500

    Databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,15 +6,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-   recipes: Recipe[] = [
-     new Recipe('a test', 'very good', 'https://www.skinnytaste' +
-       '.com/wp-content/uploads/2012/01/Stuffed-Cabbage-Casserole_-4.jpg'),
-     new Recipe('a test', 'very good', 'https://www.skinnytaste' +
-       '.com/wp-content/uploads/2012/01/Stuffed-Cabbage-Casserole_-4.jpg')];
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+     new Recipe('Egg Salad', 'very good for Health & light Break-fast', 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&auto=format&fit=crop&w=500&q=60\''),
+     new Recipe('Sprout Salad', 'very Healthy & Heavy food for Protein Consumer', 'https://images.unsplash.com/photo-1511690656952-34342bb7c2f2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60')];
+  jpg;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
