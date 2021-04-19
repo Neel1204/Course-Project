3d2a9d63b1bcc4a17f27e4bca625ea583f00218b
Neel1204 <neel9770635705@gmail.com>
2021-04-18 19:55:50 -0500

    Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe';
+  // loadedFeature = 'recipe';
 
   // tslint:disable-next-line:typedef
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
+ // onNavigate(feature: string) {
+   // this.loadedFeature = feature;
+  // }
 }
 
